13fa9bbb03ce762a3bad26ec0b22bda5473e6f38
Olivier El Mekki <oelmekki@gmail.com>
2011-08-18 12:52:42 +0200

    REMOVE uneeded files for engine

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sociopathor::Application.config.secret_token = 'ea0d355f2e241fafe210e256f0e5a75d24bfc9c54d97b3ce33fb4e971aeac59aa059323f86390e109d24a46e6caa33946cdc2850df513d9c8745d073309f5c72'
